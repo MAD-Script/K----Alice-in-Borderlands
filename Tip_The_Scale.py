91ba5fe380e63515f9600562c2d1daae766ccaf8
Adrian Dip <adm.rox@outlook.com>
2022-12-29 14:52:03 +0600

    K♦ From alice in borderlands

---
+++ Tip_The_Scale.py
@@ -0,0 +1,80 @@
+     
+def run_game(inputs):
+    sum = 0
+    for i in range(len(inputs)):
+        sum += inputs[i] 
+    avg = sum / len(inputs)
+    game = avg*0.8
+
+
+    print("The game number is: ", game , "\n")
+    #  find the closest number to the game
+
+
+    winner_index = [False]*len(inputs)
+    
+    closest = inputs[0]
+    for i in range(len(inputs)):
+        if abs(inputs[i]-game) < abs(closest-game):
+            closest = inputs[i]
+    
+
+    print("The closest number to the game is: ", closest)    
+    for i in range(len(inputs)):
+        if inputs[i] == closest:
+            winner_index[i] = True
+            
+    print("\n")        
+    return winner_index
+
+player_name = ["A","B","C","D","E"]
+player_score = [0,0,0,0,0]
+player_inputs = [0,0,0,0,0]
+
+
+# each player inputs a number to store in player_inputs
+
+# run game until all players except one have -10 score,
+# each player loses 1 point except the winner 
+# if a player has -10 score, they are out of the game
+
+while len(player_name) > 1:
+    print("\n")
+    for i in range(len(player_inputs)):
+        player_inputs[i] = int(input("Player " + player_name[i] + " enter a number: "))
+    
+    winners = run_game(player_inputs)
+    if winners.count(True) == len(player_name):
+        print("No one wins this round")
+    else:
+        for i in range(len(winners)):
+            if winners[i]:
+                print("The winner is: ", player_name[i])
+            else:
+                player_score[i] -= 1
+        
+    # for i in range(len(player_score)):
+    #     if i != winner:
+    #         player_score[i] -= 1
+    
+    #print player score and names
+    # for i in range(len(player_score)):
+    #     if player_score[i] == -3:
+    #         print(player_name[i], "is out of the game")
+    #         player_name.pop(i)
+    #         player_score.pop(i)
+    #         player_inputs.pop(i)
+    count = 0
+    while count < len(player_score):
+        if player_score[count] == -3:
+            print(player_name[count], "is out of the game")
+            player_name.pop(count)
+            player_score.pop(count)
+            player_inputs.pop(count)
+        else:
+            print(player_name[count], ": ", player_score[count]) 
+            count += 1
+        
+
+print("\nThe Ultimate Winner is: ", player_name[0])    
+    
